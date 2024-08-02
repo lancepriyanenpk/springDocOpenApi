e786d3b0cd7b01ebfe049c63e581627a1a928e20
Lance Priyan F <lancepriyan@ults.ulccs>
2024-08-02 17:15:59 +0530

    initial commit

---
+++ src/test/java/com/example/springDocOpenApi/SpringDocOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDocOpenApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDocOpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
